92da5ceae823cddc363b8f1e2d86650df0c30baa
MD Arif Islam <67411663+arifislamarif344@users.noreply.github.com>
2020-08-30 09:21:37 +0600

    Update auth.php

--- myCrud/auth.php
+++ myCrud/auth.php
@@ -5,6 +5,8 @@ session_start([
     'cookie_lifetime' => 300, //5 minutes
 ]);
 
+error_reporting(0);
+
 $error = false;
 $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
 $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
